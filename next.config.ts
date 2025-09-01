f1e4aef454400f969b6ab7d1023683c1ba70eb8c
Estefania Pérez Vázquez <perezvazquezestefania41@gmail.com>
2025-09-01 13:06:47 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -28,6 +29,13 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+    ],
+  },
+  experimental: {
+    // This is required to allow the Next.js dev server to accept requests from
+    // the Firebase Studio preview server.
+    allowedDevOrigins: [
+      '6000-firebase-studio-1756573387385.cluster-f73ibkkuije66wssuontdtbx6q.cloudworkstations.dev',
     ],
   },
 };
