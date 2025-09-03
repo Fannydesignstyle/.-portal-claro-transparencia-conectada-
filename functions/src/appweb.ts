917177b574b4d8b97783546bfffaec42c178e09c
Estefania Pérez Vázquez <perezvazquezestefania41@gmail.com>
2025-09-03 11:30:17 +0000

    feat: Actualizar importaciones de Firebase Functions a v2 y activar helloWorld

--- functions/src/appweb.ts
+++ functions/src/appweb.ts
@@ -8,7 +8,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {setGlobalOptions} from "firebase-functions";
+import {setGlobalOptions} from "firebase-functions/v2";
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
@@ -27,7 +27,7 @@ setGlobalOptions({ maxInstances: 10 });
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
